6c0d3f7d676e370d8008a27bd13a0d265b7e223b
Julia Fokanova <oncologmma@mail.ru>
2024-07-18 12:27:46 +0300

    add label

--- src/js/Form.js
+++ src/js/Form.js
@@ -1,6 +1,7 @@
 export default class Form {
   // <form class="form">
-  //   <input class="form__input" type="text" placeholder="Введите номер карты">
+  //   <label class="form__label visually-hidden" for="cardNumber">Введите номер карты</label>
+  //   <input class="form__input" id="cardNumber" type="text" placeholder="Введите номер карты">
   //   <div class="form__tooltip hidden"></div>
   //   <button class="form__button" type="submit">Click to Validate</button>
   // </form>
@@ -8,8 +9,14 @@ export default class Form {
     this.element = document.createElement('form');
     this.element.classList.add('form');
 
+    this.labelElement = document.createElement('label');
+    this.labelElement.classList.add('form__label', 'visually-hidden');
+    this.labelElement.for = 'cardNumber';
+    this.labelElement.textContent = 'Введите номер карты';
+
     this.inputElement = document.createElement('input');
     this.inputElement.classList.add('form__input');
+    this.inputElement.id = 'cardNumber';
     this.inputElement.type = 'text';
     this.inputElement.placeholder = 'Введите номер карты';
 
@@ -21,6 +28,11 @@ export default class Form {
     this.buttonElement.type = 'submit';
     this.buttonElement.textContent = 'Click to Validate';
 
-    this.element.append(this.inputElement, this.tooltipElement, this.buttonElement);
+    this.element.append(
+      this.labelElement,
+      this.inputElement,
+      this.tooltipElement,
+      this.buttonElement,
+    );
   }
 }
