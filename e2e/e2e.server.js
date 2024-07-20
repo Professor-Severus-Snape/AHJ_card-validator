472227cc74d208a001f847de08a016467b011087
Damir Rysaev <r.damir@zdravicity.ru>
2024-07-20 11:27:01 +0300

    fix e2e.server.js

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -1,11 +1,13 @@
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('../webpack.config');
+const generateConfig = require('../webpack.config');
+
+const config = generateConfig({ env: { development: true }})
 
 const PORT = 9000;
 
 const compiler = Webpack(config);
-const devServerOptions = { ...config.devServer, open: true, port: PORT };
+const devServerOptions = { ...config.devServer, open: false, port: PORT };
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 const runServer = async () => {
